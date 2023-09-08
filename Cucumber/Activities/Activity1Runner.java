d701cc753dffd005e727fe4e87e767e3bbe1573f
Vaishali2508 <kumarinishu90@gmail.com>
2023-09-08 17:20:23 +0530

    Add files via upload

---
+++ Cucumber/Activities/Activity1Runner.java
@@ -0,0 +1,14 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "features",
+        glue = {"stepDefinations"},
+        tags = "@activity1"
+)
+public class Activity1Runner {
+
+
+}
---
+++ Cucumber/Activities/Activity2Runner.java
@@ -0,0 +1,12 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "features",
+        glue = {"stepDefinitions"},
+        tags = "@activity2"
+)
+public class Activity2Runner {
+}
